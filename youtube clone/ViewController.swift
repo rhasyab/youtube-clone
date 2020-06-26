175e5ad6feb26d245fbd329b00faa375e5bc8c73
Rhasya Bintang Rizqi <rhasyabintangrizqi@MacBook-Pro-Rhasya.local>
2020-06-26 23:57:42 +0700

    Message: Initial xcode file

---
+++ youtube clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube clone
+//
+//  Created by Rhasya Bintang Rizqi on 26/06/20.
+//  Copyright © 2020 Rhasya Bintang Rizqi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
